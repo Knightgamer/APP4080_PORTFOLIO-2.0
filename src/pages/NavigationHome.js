3bed17cf299180b383db434a67a2a9129dee3c8f
iWB-Git <sbp1784@gmail.com>
2023-11-17 14:54:51 +0300

    changed the Profile code for the navigation

--- src/pages/NavigationHome.js
+++ src/pages/NavigationHome.js
@@ -6,6 +6,7 @@ import {
   faUserCircle,
   faCog,
   faSignOutAlt,
+  faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import particlesConfigDark from "../particles.json";
 import particlesConfigLight from "../particleslight.json";
@@ -18,7 +19,11 @@ function Navigation() {
   const [theme, setTheme] = useState("light");
   const [particlesConfig, setParticlesConfig] = useState(particlesConfigLight);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [user, setUser] = useState(null); // State to store user information
 
+  const handleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
   useEffect(() => {
     document.documentElement.classList.remove("dark", "light");
     document.documentElement.classList.add(theme);
@@ -26,7 +31,23 @@ function Navigation() {
       theme === "dark" ? particlesConfigDark : particlesConfigLight
     );
   }, [theme]);
+  useEffect(() => {
+    // Subscribe to auth state changes
+    const unsubscribe = auth.onAuthStateChanged((currentUser) => {
+      setUser(currentUser);
+    });
 
+    return () => unsubscribe(); // Unsubscribe on cleanup
+  }, []);
+  // Function to get the first initial
+  const getInitial = () => {
+    if (user) {
+      return user.displayName
+        ? user.displayName.charAt(0)
+        : user.email.charAt(0);
+    }
+    return ""; // Return empty string if no user
+  };
   const handleThemeSwitch = () => {
     setTheme(theme === "dark" ? "light" : "dark");
   };
@@ -58,29 +79,61 @@ function Navigation() {
         >
           <FontAwesomeIcon icon={theme === "dark" ? faSun : faMoon} />
         </button>
-        <div className="relative">
-          <FontAwesomeIcon
-            icon={faUserCircle}
-            className="text-2xl cursor-pointer"
-            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-          />
-          {isDropdownOpen && (
-            <div className="absolute right-0 mt-2 py-2 w-48 bg-white dark:bg-gray-700 rounded-lg shadow-xl">
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600"
-              >
-                <FontAwesomeIcon icon={faCog} className="pr-1" /> Settings
-              </a>
-              <a
-                href="#"
-                onClick={handleLogout}
-                className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600"
-              >
-                <FontAwesomeIcon icon={faSignOutAlt} className="pr-1" /> Logout
-              </a>
-            </div>
-          )}
+        <div classame="relative">
+          <li className="list-none relative">
+            <a
+              href="#"
+              className="flex items-center space-x-2 cursor-pointer"
+              onClick={handleDropdown}
+            >
+              {/* User Avatar */}
+              <img
+                src={`https://ui-avatars.com/api/?name=${user?.email}&background=E93B81&color=fff`}
+                className="w-8 h-8 rounded-full"
+                alt="User Avatar"
+              />
+            </a>
+            {isDropdownOpen && (
+              <div className="absolute right-0 mt-2 py-3 bg-white dark:bg-gray-900  rounded shadow-xl w-48">
+                <div className="flex flex-col items-center px-4 py-3">
+                  <img
+                    src={`https://ui-avatars.com/api/?name=${user?.email}&background=E93B81&color=fff`}
+                    className="w-20 h-20 rounded-full"
+                    alt="User Avatar"
+                  />
+                  <div className="text-center">
+                    <p className="mt-2 text-gray-800 dark:text-gray-200 text-sm">
+                      {user?.email}
+                    </p>
+                  </div>
+                </div>
+                <ul className="list-none p-2">
+                  <li className="py-2">
+                    <a
+                      href="#"
+                      className="flex items-center px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600"
+                    >
+                      <FontAwesomeIcon
+                        icon={faUser}
+                        className="mr-2 background-color=[#E93B81]"
+                      />
+                      Profile
+                    </a>
+                  </li>
+                  <li className="py-2">
+                    <a
+                      href="#"
+                      onClick={handleLogout}
+                      className="flex items-center px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600"
+                    >
+                      <FontAwesomeIcon icon={faSignOutAlt} className="mr-2" />
+                      Logout
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </li>
         </div>
       </div>
       <Particles options={particlesConfig} />
