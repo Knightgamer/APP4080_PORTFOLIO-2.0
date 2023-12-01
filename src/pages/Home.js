106a904c7ee4026847e26ef876ec736ac06aabb2
iWB-Git <sbp1784@gmail.com>
2023-12-01 18:55:34 +0300

    Update code to use functional component syntax

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,18 @@ import React, { useState } from "react";
 import GitHubProfile from "./GitHubProfile";
 import Navigation from "./NavigationHome";
 import ProfileCard from "./ProfileCard";
+
 const Home = () => {
-    const [githubUsername, setGithubUsername] = useState("");
+  const [githubUsername, setGithubUsername] = useState("");
 
   return (
     <div>
-      <div>
-        <Navigation />
-      </div>
-      <div className="grid grid-cols-12 gap-4 md:gap-6 lg:gap-8 lg:mt-[220px]">
-        <div className="col-span-12 md:col-span-4 h-screen">
+      <Navigation />
+      <div className="grid grid-cols-1 md:grid-cols-12 gap-4 md:gap-6 lg:gap-8 lg:mt-[220px]">
+        <div className="col-span-1 md:col-span-4 h-auto md:h-screen">
           <ProfileCard setGithubUsername={setGithubUsername} />
         </div>
-        <div className="col-span-12 md:col-span-8">
+        <div className="col-span-1 md:col-span-8">
           <GitHubProfile githubUsername={githubUsername} />
         </div>
       </div>
